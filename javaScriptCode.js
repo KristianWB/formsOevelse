73238481e4d4e10768acd64035c9704e236339e2
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-02 12:09:34 +0200

    javaScript code added

---
+++ javaScriptCode.js
@@ -0,0 +1,9 @@
+let loginObjekt = {};
+let lead = document.getElementById("pre");
+let email = document.getElementById("exampleInputEmail1");
+
+f = () => {
+  if (email.value) {
+    pre.innerHTML = pre.innerHTML + "E-mail: " + email.value + "\n # ";
+  }
+};
